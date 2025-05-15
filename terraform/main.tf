b5c0eaef6c4a03983999b792904982c858e7900a
YenTranSl <tranthikimyen27052004@gmail.com>
2025-05-16 00:00:49 +0700

    update comment of file main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,10 @@
-# Cau hinh Provider AWS
+# Configure AWS Provider
 provider "aws" {
   profile = "se400"
   region  = var.region
 }
 
-# Tao cap khoa SSH
-resource "aws_key_pair" "se400" {
-  key_name   = "se400_key_pair"
-  public_key = file("../.ssh/id_rsa.pub")
-}
-
-# Tao may chu EC2
+# Create EC2 Instance
 resource "aws_instance" "se400" {
   ami           = var.instance_ami
   instance_type = var.instance_type
@@ -20,6 +14,12 @@ resource "aws_instance" "se400" {
     Name = var.instance_name
   }
   vpc_security_group_ids = [aws_security_group.note_sg.id]
+}
+
+# Create Security Group with required access
+resource "aws_key_pair" "se400" {
+  key_name   = "se400_key_pair"
+  public_key = file("../.ssh/id_rsa.pub")
 }
 
 resource "aws_security_group" "note_sg" {
